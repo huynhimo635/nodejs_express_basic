04b5647abd8d800ac21bf1c4c6e2b4290b4a0f68
huynhimo635 <huynh.nguyen.imo@gmail.com>
2023-05-15 10:51:25 +0700

    build: add morgan lib to show HTTP logger
    Learn how to see the HTTP logger in express via morgan lib

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-express = require('express')
-app = express()
-port = 3000
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.get('/test1', (req, res) => {
